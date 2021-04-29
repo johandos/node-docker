1357f6330dc83749903e16470ffa6c6cf11dd64a
Johan Ospina <johan@kaavan.es>
2021-04-29 08:34:41 +0200

    Feature: textos no necesarios
    Modificacion en los textos para dejar los necesarios y funcionales

--- search-ng/src/app/components/beers/beers.component.ts
+++ search-ng/src/app/components/beers/beers.component.ts
@@ -16,13 +16,4 @@ export class BeersComponent implements OnInit {
     this.http.get('https://api.punkapi.com/v2/beers')
       .subscribe(res => (this.beers = res));
   }
-  Search(){
-    if (this.filtered === ""){
-      this.ngOnInit();
-    }else{
-      this.beers = this.beers.filter(item => {
-        return item.name.toLocaleLowerCase().includes(this.filtered);
-      });
-    }
-  }
 }
